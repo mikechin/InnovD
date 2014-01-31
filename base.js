79aab0fd98da8307524300fcfbe9f59656e1aabc
mike chin <mike.chin@concet3D.com>
2014-01-30 23:57:25 -0700

    add link to purchase page.

--- base.js
+++ base.js
@@ -2,4 +2,8 @@ $(window).load(function() {
 	$('.button[data-action="download"]').on('click', function(event) {
 		window.location.href = 'assets/downloads/move+resize.zip';
 	});
+	$('.button[data-action="buy"]').on('click', function(event) {
+		var win = window.open('https://sites.fastspring.com/innovationdistrict/instant/moveresize', '_blank');
+		win.focus();
+	});
 });
